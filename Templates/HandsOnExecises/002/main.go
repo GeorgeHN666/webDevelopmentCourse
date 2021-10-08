6f8baa3c2eaf6c41ac115c452a87cae7e4f12b2f
GeorgeHN666 <jorge1029hsgmail.com>
2021-10-08 23:33:03 +0200

    .

---
+++ Templates/HandsOnExecises/002/main.go
@@ -0,0 +1,13 @@
+package main
+
+type hotels struct {
+	Name, Address, City, Region string
+	Zip                         int
+}
+type region struct {
+	Southern, Central, Northern []hotels
+}
+
+func main() {
+
+}
